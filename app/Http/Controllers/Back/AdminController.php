08357bf0c08620523252cab38970b9c113044b1a
wujianhua <522025327@qq.com>
2018-07-28 00:19:44 +0800

    修改登录css

--- app/Http/Controllers/Back/AdminController.php
+++ app/Http/Controllers/Back/AdminController.php
@@ -5,17 +5,11 @@ namespace App\Http\Controllers\Back;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class AdminController extends Controller
+class AdminController extends CommonController
 {
-    public function __construct()
-    {
-//        $this->middleware('login');
-    }
 
     public function index()
     {
 
-        dd(config('route.front_url'));
-        //        dd(111);
     }
 }
--- storage/framework/views/4841dd2351621d00234fb069b04b6c0f7b6ba46c.php
+++ storage/framework/views/4841dd2351621d00234fb069b04b6c0f7b6ba46c.php
@@ -14,31 +14,60 @@
 </head>
 <body>
 <div id="loginbox">
-    <form id="loginform" method="post" class="form-vertical" action="<?php echo e(url('login/login')); ?>">
+    
+    <?php if($errors->any()): ?>
+        <div class="alert alert-danger">
+            <ul>
+                <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <li><?php echo e($error); ?></li>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+    
+
+    
+    <?php echo e(session('user.username')); ?>
+
+    <form class="form-horizontal" method="post" action="<?php echo e(url('login/login')); ?>" name="basic_validate" id="basic_validate" novalidate="novalidate">
         <?php echo csrf_field(); ?>
         <div class="control-group normal_text"><h3><img src="<?php echo e(URL::asset('/back/img/logo.png')); ?>" alt="Logo"/></h3>
         </div>
         <div class="control-group">
             <div class="controls">
                 <div class="main_input_box">
-                    <span class="add-on bg_lg"><i class="icon-user"></i></span><input name="username" type="text" placeholder="用户名"/>
+                    <span class="add-on bg_lg"><i class="icon-user"></i></span><input name="username" type="text" placeholder="用户名" class="required"/>
                 </div>
             </div>
         </div>
+
         <div class="control-group">
             <div class="controls">
                 <div class="main_input_box">
-                    <span class="add-on bg_ly"><i class="icon-lock"></i></span><input name="password" type="password" placeholder="密码"/>
+                    <span class="add-on bg_ly"><i class="icon-lock"></i></span><input name="password" type="password" placeholder="密码" class="required"/>
                 </div>
             </div>
         </div>
+
+        <div class="control-group">
+            <div class="controls">
+                <div class="main_input_box">
+                    <img src="<?php echo e(captcha_src()); ?>" style="cursor: pointer;margin-bottom: 4px;" onclick="this.src='<?php echo e(captcha_src()); ?>'+Math.random()">
+                    <input type="text" style="width: 60%" class="required form-control <?php echo e($errors->has('captcha')?'parsley-error':''); ?>" name="captcha" placeholder="验证码">
+                </div>
+            </div>
+        </div>
+
         <div class="form-actions">
             <span class="pull-left"><a href="#" class="flip-link btn btn-info" id="to-recover">忘记密码?</a></span>
             <span class="pull-right">
-                <button type="submit" class="btn btn-success">登录</button>
+                
+                <input type="submit" value="Save" class="btn btn-success">
             </span>
+
         </div>
     </form>
+    
     <form id="recoverform" action="#" class="form-vertical">
         <p class="normal_text">请输入正确的用户名密码.</p>
 
@@ -57,6 +86,14 @@
 
 <script src="<?php echo e(URL::asset('/back/js/jquery.min.js')); ?>"></script>
 <script src="<?php echo e(URL::asset('/back/js/matrix.login.js')); ?>"></script>
+
+<script src="<?php echo e(URL::asset('/back/js/bootstrap.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('/back/js/matrix.js')); ?>"></script>
+
+<script src="<?php echo e(URL::asset('/back/js/jquery.uniform.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('/back/js/select2.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('/back/js/jquery.validate.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('/back/js/matrix.form_validation.js')); ?>"></script>
 </body>
 
 </html>
