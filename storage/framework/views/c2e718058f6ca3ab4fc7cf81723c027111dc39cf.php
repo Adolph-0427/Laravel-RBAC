85dedb0fe07bb1702843865a431eabdeb506421c
wujianhua <522025327@qq.com>
2018-08-01 00:29:21 +0800

    整理插件

--- storage/framework/views/c2e718058f6ca3ab4fc7cf81723c027111dc39cf.php
+++ storage/framework/views/c2e718058f6ca3ab4fc7cf81723c027111dc39cf.php
@@ -6,27 +6,7 @@
     <link rel="stylesheet" href="<?php echo e(URL::asset('/back/css/datepicker.css')); ?>"/>
     <link rel="stylesheet" href="<?php echo e(URL::asset('/back/css/uniform.css')); ?>"/>
     <link rel="stylesheet" href="<?php echo e(URL::asset('/back/css/select2.css')); ?>"/>
-
-    
-    <!-- Include external CSS. -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet"
-          type="text/css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/codemirror.min.css">
-    <!-- Include Editor style. -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.8.4/css/froala_editor.pkgd.min.css"
-          rel="stylesheet" type="text/css"/>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.8.4/css/froala_style.min.css" rel="stylesheet"
-          type="text/css"/>
-    
-    <link href="https://cdn.bootcss.com/webuploader/0.1.1/webuploader.css" rel="stylesheet">
 <?php $__env->stopSection(); ?>
-<style type="text/css">
-    a[href="https://froala.com/wysiwyg-editor"], a[href="https://www.froala.com/wysiwyg-editor?k=u"] {
-        display: none !important;
-        position: absolute;
-        top: -99999999px;
-    }
-</style>
 <?php $__env->startSection('content'); ?>
     <div class="container-fluid">
         <hr/>
@@ -38,7 +18,8 @@
                         <h5>添加文章</h5>
                     </div>
                     <div class="widget-content nopadding">
-                        <form class="form-horizontal" method="post" action="<?php echo e(url('articles')); ?>" name="basic_validate"
+                        <form class="form-horizontal" method="post" action="<?php echo e(url('articles')); ?>"
+                              name="basic_validate"
                               id="basic_validate" novalidate="novalidate">
                             <?php echo csrf_field(); ?>
                             <div class="control-group">
@@ -59,9 +40,7 @@
                                     <div id="uploader-demo">
                                         <!--用来存放item-->
                                         <div id="fileList" class="uploader-list"></div>
-                                        <div id="filePicker">
-                                            选择图片
-                                        </div>
+                                        <div id="filePicker"> 选择图片</div>
                                     </div>
                                     <input type="hidden" name="cover_img" class="required"/>
                                 </div>
@@ -85,8 +64,10 @@
                             <div class="control-group">
                                 <label class="control-label">发表时间</label>
                                 <div class="controls">
-                                    <div data-date="12-02-2012" class="input-append date datepicker">
-                                        <input type="text" value="<?php echo e(\Carbon\Carbon::now()->toDateString()); ?>"
+                                    <div data-date="12-02-2012"
+                                         class="input-append date datepicker">
+                                        <input type="text"
+                                               value="<?php echo e(\Carbon\Carbon::now()->toDateString()); ?>"
                                                class="span11" data-date-format="mm-dd-yyyy"/>
                                         <span class="add-on"><i class="icon-th"></i></span>
                                     </div>
@@ -100,7 +81,8 @@
                                 <div class="control-group">
                                     <label class="control-label">内容</label>
                                     <div class="controls" style="padding-right: 10%">
-                                        <textarea id="edit" class="textarea_editor span12" rows="6"></textarea>
+                                        <textarea id="edit" class="textarea_editor span12"
+                                                  rows="6"></textarea>
                                     </div>
                                 </div>
                                 <div class="form-actions">
@@ -120,7 +102,6 @@
             return true;
         }
     </script>
-
     <script src="<?php echo e(URL::asset('/back/js/jquery.uniform.js')); ?>"></script>
     <script src="<?php echo e(URL::asset('/back/js/select2.min.js')); ?>"></script>
     <script src="<?php echo e(URL::asset('/back/js/jquery.validate.js')); ?>"></script>
@@ -130,64 +111,12 @@
     <script src="<?php echo e(URL::asset('/back/js/bootstrap-colorpicker.js')); ?>"></script>
     <script src="<?php echo e(URL::asset('/back/js/bootstrap-datepicker.js')); ?>"></script>
 
-    
-    <!-- Include external JS libs. -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/codemirror.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/mode/xml/xml.min.js"></script>
-    <!-- Include Editor JS files. -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.8.4/js/froala_editor.pkgd.min.js"></script>
-    <script src="https://cdn.bootcss.com/froala-editor/2.8.1/js/languages/zh_cn.js" type="text/javascript"></script>
-
-    <script>
-        var _$ = jQuery.noConflict(true);
-        _$(function () {
-            _$('#edit').froalaEditor({
-                placeholderText: '请输入内容',
-                imageUploadURL: "<?php echo e(url('articles/articleCover')); ?>",//上传图片路径
-                enter: _$.FroalaEditor.ENTER_BR,
-                language: 'zh_cn',
-                height: 150,
-                toolbarButtons: [
-                    'bold', 'italic', 'underline', 'paragraphFormat', 'align', 'color', 'fontSize', 'insertImage', 'insertTable', 'undo', 'redo'
-                ]
-            })
-        });
-    </script>
-
-    
-    <script src="https://cdn.bootcss.com/jquery/1.12.3/jquery.min.js"></script>
-    <script type="text/javascript" src="http://cdn.staticfile.org/webuploader/0.1.0/webuploader.js"></script>
-
-    // swf文件路径
-    <script>
-        // 初始化Web Uploader
-        var jquery = jQuery.noConflict(true);
-        var uploader = WebUploader.create({
-
-            // 选完文件后，是否自动上传。
-            auto: true,
-            formData:{
-                _token:'<?php echo e(csrf_token()); ?>',
-                name:'articleCover',
-            },
-            // swf文件路径
-            swf: 'https://cdn.bootcss.com/webuploader/0.1.0/Uploader.swf',
-
-            // 文件接收服务端。
-            server: "<?php echo e(url('articles/articleCover')); ?>",
-
-            // 选择文件的按钮。可选。
-            // 内部根据当前运行是创建，可能是input元素，也可能是flash.
-            pick: '#filePicker',
-
-            // 只允许选择图片文件。
-            accept: {
-                title: 'Images',
-                extensions: 'gif,jpg,jpeg,bmp,png',
-                mimeTypes: 'image/*'
-            }
-        });
-    </script>
 <?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('Back.Common.webUpload',[
+    'server'=>'articles/articleCover',
+    'pick'=>'filePicker'
+], array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+
+<?php echo $__env->make('Back.Common.froalaEditor', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
 <?php echo $__env->make('Back.Common.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
