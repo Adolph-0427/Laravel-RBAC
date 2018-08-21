191ace7f4422637bb27db8ff3a26c8e06f1458b4
Adolph <522025327@qq.com>
2018-08-21 18:00:01 +0800

    artisan

--- app/Console/Commands/MakeRepositoryCommand.php
+++ app/Console/Commands/MakeRepositoryCommand.php
@@ -2,242 +2,72 @@
 
 namespace App\Console\Commands;
 
-use Config;
-use Illuminate\Console\Command;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Composer;
+use Illuminate\Console\GeneratorCommand;
 
-class MakeRepositoryCommand extends Command
+class RepositoryMakeCommand extends GeneratorCommand
 {
     /**
-     * The name and signature of the console command.
+     * The console command name.
      *
      * @var string
      */
-    protected $signature = 'make:repository {repository} {--model=}';
+    protected $name = 'make:repository {--model=}';
+
+    
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Make a repository and interface';
+    protected $description = 'Create a new repository class';
 
     /**
-     * @var
-     */
-    protected $repository;
-
-    /**
-     * @var
-     */
-    protected $model;
-
-    /**
-     * Create a new command instance.
+     * The type of class being generated.
      *
-     * @param Filesystem $filesystem
-     * @param Composer $composer
+     * @var string
      */
-    public function __construct(Filesystem $filesystem, Composer $composer)
-    {
-        parent::__construct();
+    protected $type = 'Repository';
 
-        $this->files = $filesystem;
-        $this->composer = $composer;
-    }
 
     /**
-     * Execute the console command.
+     * Get the stub file for the generator.
      *
-     * @return mixed
+     * @return string
      */
-    public function handle()
+    protected function getStub()
     {
-        //获取repository和model两个参数值
-        $argument = $this->argument('repository');
-        $option = $this->option('model');
-        //自动生成RepositoryInterface和Repository文件
-        $this->writeRepository($argument, $option);
-        //重新生成autoload.php文件
-        $this->composer->dumpAutoloads();
-    }
-
-    private function writeRepository($repository, $model)
-    {
-        if ($this->createRepository($repository, $model)) {
-            //若生成成功,则输出信息
-            $this->info('Success to make a ' . ucfirst($repository) . ' Repository and a ' . ucfirst($repository) . 'Interface Interface');
-        }
-    }
-
-    private function createRepository($repository, $model)
-    {
-        // getter/setter 赋予成员变量值
-        $this->setRepository($repository);
-        $this->setModel($model);
-        // 创建文件存放路径, Repository个人一般放在app/Repositories/
-        $this->createDirectory();
-        // 生成两个文件
-        return $this->createClass();
-    }
-
-    private function createDirectory()
-    {
-        $directory = $this->getDirectory();
-        //检查路径是否存在,不存在创建一个,并赋予775权限
-        if (!$this->files->isDirectory($directory)) {
-            return $this->files->makeDirectory($directory, 0755, true);
-        }
-    }
-
-    private function getDirectory()
-    {
-        return Config::get('repository.directory_eloquent_path');
-    }
-
-    private function createClass()
-    {
-        //渲染模板文件,替换模板文件中变量值
-        $templates = $this->templateStub();
-        $class = null;
-        $class = $this->files->put($this->getPath(), $templates);
-        return $class;
-    }
-
-    private function getPath($class)
-    {
-        // 两个模板文件,对应的两个路径
-        $path = null;
-
-        $path = $this->getDirectory() . DIRECTORY_SEPARATOR . $this->getRepositoryName() . '.php';
-        return $path;
-    }
-
-    private function getInterfaceDirectory()
-    {
-        return Config::get('repository.directory_path');
-    }
-
-    private function getRepositoryName()
-    {
-        // 根据输入的repository变量参数,是否需要加上'Repository'
-        $repositoryName = $this->getRepository();
-        if ((strlen($repositoryName) < strlen('Repository')) || strrpos($repositoryName, 'Repository', -11)) {
-            $repositoryName .= 'Repository';
-        }
-        return $repositoryName;
-    }
-
-    private function getInterfaceName()
-    {
-        return $this->getRepositoryName() . 'Interface';
+        return '.\resources\stubs\Repository\repository.stub';
     }
 
     /**
-     * @return mixed
+     * Get the default namespace for the class.
+     *
+     * @param  string $rootNamespace
+     * @return string
      */
-    public function getRepository()
+    protected function getDefaultNamespace($rootNamespace)
     {
-        return $this->repository;
+        return $rootNamespace . '\Repositories';
     }
 
     /**
-     * @param mixed $repository
+     * Replace the namespace for the given stub.
+     *
+     * @param  string $stub
+     * @param  string $name
+     * @return $this
      */
-    public function setRepository($repository)
+    protected function replaceNamespace(&$stub, $name)
     {
-        $this->repository = $repository;
-    }
 
-    /**
-     * @return mixed
-     */
-    public function getModel()
-    {
-        return $this->model;
-    }
+        $stub = str_replace(
+            ['RepositoryNamespace', 'ModelName'],
+            [$this->getNamespace($name), 'AAA'],
+            $stub
+        );
 
-    /**
-     * @param mixed $model
-     */
-    public function setModel($model)
-    {
-        $this->model = $model;
-    }
-
-    private function templateStub()
-    {
-        // 获取两个模板文件
-        $stubs = $this->getStub();
-        // 获取需要替换的模板文件中变量
-        $templateData = $this->getTemplateData();
-        $renderStubs = [];
-        foreach ($stubs as $key => $stub) {
-            // 进行模板渲染
-            $renderStubs[$key] = $this->getRenderStub($templateData, $stub);
-        }
-
-        return $renderStubs;
-    }
-
-    private function getStub()
-    {
-        $stubs = [
-            'Eloquent' => $this->files->get(resource_path('stubs\Repository') . DIRECTORY_SEPARATOR . 'repository.stub'),
-        ];
-        return $stubs;
-    }
-
-    private function getTemplateData()
-    {
-        $repositoryNamespace = Config::get('repository.repository_namespace');
-        $modelNamespace = 'App\\' . $this->getModelName();
-        $repositoryInterfaceNamespace = Config::get('repository.repository_interface_namespace');
-        $repositoryInterface = $this->getInterfaceName();
-        $className = $this->getRepositoryName();
-        $modelName = $this->getModelName();
-
-        $templateVar = [
-            'repository_namespace' => $repositoryNamespace,
-            'model_namespace' => $modelNamespace,
-            'repository_interface_namespace' => $repositoryInterfaceNamespace,
-            'repository_interface' => $repositoryInterface,
-            'class_name' => $className,
-            'model_name' => $modelName,
-            'model_var_name' => strtolower($modelName),
-        ];
-
-        return $templateVar;
-    }
-
-    private function getRenderStub($templateData, $stub)
-    {
-        foreach ($templateData as $search => $replace) {
-            $stub = str_replace('$' . $search, $replace, $stub);
-        }
-
-        return $stub;
-    }
-
-    private function getModelName()
-    {
-        $modelName = $this->getModel();
-        if (isset($modelName) && !empty($modelName)) {
-            $modelName = ucfirst($modelName);
-        } else {
-            // 若option选项没写,则根据repository来生成Model Name
-            $modelName = $this->getModelFromRepository();
-        }
-
-        return $modelName;
-    }
-
-    private function getModelFromRepository()
-    {
-        $repository = strtolower($this->getRepository());
-        $repository = str_replace('repository', '', $repository);
-        return ucfirst($repository);
+        return $this;
     }
 
 }
