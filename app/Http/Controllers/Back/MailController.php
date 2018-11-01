55470778ab3513a69ea8689b4d5761ee95f4ef81
Mr wu <522025327@qq.com>
2018-11-01 16:56:26 +0800

    Update MailController.php

--- app/Http/Controllers/Back/MailController.php
+++ app/Http/Controllers/Back/MailController.php
@@ -8,30 +8,6 @@ use App\Http\Controllers\Controller;
 
 class MailController extends Controller
 {
-
-    const mailAccount = 'wujianhua0427@qq.com';//邮件账户
-    const mailPasswd = 'vnqfkzubqpvnbjha';//邮件密码
-    const mailAddress = 'wujianhua0427@qq.com';//邮件地址
-    const mailServer = 'imap.qq.com';//邮件服务器
-    const serverType = 'pop3';//服务器类型
-    const port = '110';//端口号
-    protected $savePath;//附件存储地址
-    protected $webPath;//网页地址
-
-
-    public function receive()
-    {
-        $re = $this->pop3_login(self::mailServer, self::port, self::mailAddress, self::mailPasswd);
-        dd($re);
-    }
-
-    public function pop3_login($host, $port, $user, $pass, $folder = "INBOX", $ssl = false)
-    {
-        $ssl = ($ssl == false) ? "/novalidate-cert" : "";
-        return (@imap_open("{" . "$host:$port/pop3$ssl" . "}$folder", $user, $pass));
-    }
-
-
     /**
      * 发送邮件
      */
